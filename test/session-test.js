12b01a74efe6796dd3f031a28cbb21cfc43cbc9b
weekian <weekian.yeam@gmail.com>
2017-12-11 14:06:27 +0900

    Updated test cases to test chat-wide and chat-and-user-wide sessions

--- test/session-test.js
+++ test/session-test.js
@@ -2,14 +2,21 @@ const Telegraf = require('telegraf')
 const test = require('ava')
 const RedisSession = require('../lib/session')
 
-test.serial('should be defined', (t) => {
+test.serial('session should be defined', (t) => {
   const app = new Telegraf()
   const session = new RedisSession()
-  app.on('text', session.middleware(), (ctx) => t.true('session' in ctx))
+  app.on('text', session.middleware('session', (ctx) => ctx.from && ctx.chat && `${ctx.from.id}:${ctx.chat.id}`), (ctx) => t.true('session' in ctx))
   return app.handleUpdate({message: {chat: {id: 1}, from: {id: 1}, text: 'hey'}})
 })
 
-test.serial('should retrieve and save session', (t) => {
+test.serial('chatSession should be defined', (t) => {
+  const app = new Telegraf()
+  const session = new RedisSession()
+  app.on('text', session.middleware('chatSession', (ctx) => ctx.from && ctx.chat && `${ctx.chat.id}`), (ctx) => t.true('chatSession' in ctx))
+  return app.handleUpdate({message: {chat: {id: 1}, from: {id: 1}, text: 'hey'}})
+})
+
+test.serial('session should retrieve and save session', (t) => {
   const redisSession = new RedisSession()
   const key = '1:1'
   return redisSession.getSession(key)
@@ -27,11 +34,29 @@ test.serial('should retrieve and save session', (t) => {
     })
 })
 
-test.serial('should handle existing session', (t) => {
+test.serial('chatSession should retrieve and save session', (t) => {
+  const redisSession = new RedisSession()
+  const key = '1'
+  return redisSession.getSession(key)
+    .then((session) => {
+      t.truthy(session)
+      session.foo = 42
+      return redisSession.saveSession(key, session)
+    })
+    .then(() => {
+      return redisSession.getSession(key)
+    })
+    .then((session) => {
+      t.truthy(session)
+      t.deepEqual({ foo: 42 }, session)
+    })
+})
+
+test.serial('session should handle existing session', (t) => {
   const app = new Telegraf()
   const session = new RedisSession()
   app.on('text',
-    session.middleware(),
+    session.middleware('session', (ctx) => ctx.from && ctx.chat && `${ctx.from.id}:${ctx.chat.id}`),
     (ctx) => {
       t.true('session' in ctx)
       t.true('foo' in ctx.session)
@@ -40,27 +65,65 @@ test.serial('should handle existing session', (t) => {
   return app.handleUpdate({message: {chat: {id: 1}, from: {id: 1}, text: 'hey'}})
 })
 
-test.serial('should handle not existing session', (t) => {
+test.serial('chatSession should handle existing session', (t) => {
   const app = new Telegraf()
   const session = new RedisSession()
   app.on('text',
-    session.middleware(),
+    session.middleware('chatSession', (ctx) => ctx.from && ctx.chat && `${ctx.chat.id}`),
+    (ctx) => {
+      t.true('chatSession' in ctx)
+      t.true('foo' in ctx.chatSession)
+      t.is(ctx.chatSession.foo, 42)
+    })
+  return app.handleUpdate({message: {chat: {id: 1}, from: {id: 1}, text: 'hey'}})
+})
+
+test.serial('session should handle not existing session', (t) => {
+  const app = new Telegraf()
+  const session = new RedisSession()
+  app.on('text',
+    session.middleware('session', (ctx) => ctx.from && ctx.chat && `${ctx.from.id}:${ctx.chat.id}`),
     (ctx) => {
       t.true('session' in ctx)
       t.false('foo' in ctx.session)
     })
-  return app.handleUpdate({message: {chat: {id: 1}, from: {id: 999}, text: 'hey'}})
+  return app.handleUpdate({message: {chat: {id: 1}, from: {id: 2}, text: 'hey'}})
 })
 
-test.serial('should handle session reset', (t) => {
+test.serial('chatSession should handle not existing session', (t) => {
   const app = new Telegraf()
   const session = new RedisSession()
   app.on('text',
-    session.middleware(),
+    session.middleware('chatSession', (ctx) => ctx.from && ctx.chat && `${ctx.chat.id}`),
+    (ctx) => {
+      t.true('chatSession' in ctx)
+      t.false('foo' in ctx.chatSession)
+    })
+  return app.handleUpdate({message: {chat: {id: 2}, from: {id: 2}, text: 'hey'}})
+})
+
+test.serial('session should handle session reset', (t) => {
+  const app = new Telegraf()
+  const session = new RedisSession()
+  app.on('text',
+    session.middleware('session', (ctx) => ctx.from && ctx.chat && `${ctx.from.id}:${ctx.chat.id}`),
     (ctx) => {
       ctx.session = null
       t.truthy(ctx.session)
       t.false('foo' in ctx.session)
+    })
+  return app.handleUpdate({message: {chat: {id: 1}, from: {id: 1}, text: 'hey'}})
+})
+
+test.serial('chatSession should handle session reset', (t) => {
+  const app = new Telegraf()
+  const session = new RedisSession()
+  app.on('text',
+    session.middleware('chatSession', (ctx) => ctx.from && ctx.chat && `${ctx.chat.id}`),
+    (ctx) => {
+      ctx.chatSession = null
+      t.truthy(ctx.chatSession)
+      t.false('foo' in ctx.chatSession)
     })
   return app.handleUpdate({message: {chat: {id: 1}, from: {id: 1}, text: 'hey'}})
 })
